b9b2e5e222d145be797ed95626d288600e437c6b
Gabriel <gabnology@gmail.com>
2019-07-11 10:05:04 +0200

    New router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import TopBar from "./components/TopBar";
+import Home from "./components/Home";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
     return (
     <div>
-
+        <Router>
+            <TopBar />
+            <Switch>
+                <Route path="/" exact component={Home}/>
+            </Switch>
+        </Router>
     </div>
   );
 }
---
+++ src/components/TopBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const TopBar = () => {
+    
+    const navStyle = {
+        color: "white"
+    };
+
+    const titleStyle = {
+        color: "grey"
+    };
+
+    return (
+            <nav>
+                <h3 className="ubuntu-font" style={titleStyle}>Navigation</h3>
+                <ul className="nav-links">
+                    <Link to="/" style={navStyle}>
+                        <li>Home</li>
+                    </Link>
+                </ul>
+            </nav>
+    );
+};
+
+export default TopBar;
