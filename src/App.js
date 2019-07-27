30fca8a02494d554a70c0c8192227ae37f3fba2b
Gabriel <gabnology@gmail.com>
2019-07-27 10:12:51 +0200

    Cleared app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import TopBar from "./components/TopBar";
-import Home from "./components/Home";
-import Source from "./components/Source";
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 const App = () => {
     return (
     <div>
-        <Router>
-            <TopBar />
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/source" component={Source}/>
-            </Switch>
-        </Router>
+        
     </div>
   );
 }
