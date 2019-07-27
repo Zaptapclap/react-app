52f4b11feec0b81c965deb8009debc0463c75228
Zachary <zaptapclap+github@gmail.com>
2019-07-27 09:58:04 +0200

    Shortened home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Home = () => {
-    return (
-        <div>
-            <h1 className="lato-font">Home Page</h1>
-            <p>this is a website written by someone :)</p>
-        </div>
-    );
-};
+const Home = () => (
+    <div>
+        <h1 className="lato-font">Home Page</h1>
+        <p>this is a website written by someone dumb :)</p>
+    </div>
+);
 
 export default Home;
